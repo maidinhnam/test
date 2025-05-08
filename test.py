bed168132db2a7a2b487106343f27d7f2d2a674a
maidinhnam <maidinhnam.2001@gmail.com>
2025-05-08 21:21:10 +0200

    add

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 # Ghi nội dung vào file
-with open("session/example.txt", "w", encoding="utf-8") as f:
+with open("/session/example.txt", "w", encoding="utf-8") as f:
     f.write("Xin chào!\nĐây là một file văn bản.")
